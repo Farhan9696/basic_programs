b5728fc439b089e5b4b8ef236f232783299d6e17
Farhan9696 <37727645+Farhan9696@users.noreply.github.com>
2019-08-28 14:08:38 +0530

    Create maxNumSwitch.c

---
+++ maxNumSwitch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2;
+    printf("Enter two numbers to find maximum: ");
+    scanf("%d%d", &n1, &n2);
+    switch(n1 > n2)
+    {   
+        case 0: 
+            printf("%d is maximum", n2);
+            break;
+        case 1: 
+            printf("%d is maximum", n1);
+            break;
+    }
+
+    return 0;
+}
