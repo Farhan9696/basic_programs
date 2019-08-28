ed3d13cabadebdf5afe9c2f6e07fbdb444c20e0d
Farhan9696 <37727645+Farhan9696@users.noreply.github.com>
2019-08-28 15:02:03 +0530

    Create loop8.c

---
+++ loop8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n, sum=0;
+    printf("Enter limit: ");
+    scanf("%d", &n);
+    for(i=1; i<=n; i+=2)
+    {
+        sum += i;
+    }
+    printf("Sum of Odd Numbers between 1 to %d= %d", n, sum);
+
+    return 0;
+}
