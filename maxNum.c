88a358bba7a95356edfce6ccc6a562624afc6fe6
Farhan9696 <37727645+Farhan9696@users.noreply.github.com>
2019-08-28 09:28:54 +0530

    Create maxNum.c

---
+++ maxNum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &n1, &n2);
+    if(n1 > n2)
+    {
+        printf("%d is maximum", n1);        
+    }
+    if(n2 > n1)
+    {
+        printf("%d is maximum", n2);
+    }
+    if(n1 == n2)
+    {
+        printf("Both are equal");
+    }
+
+    return 0;
+}
