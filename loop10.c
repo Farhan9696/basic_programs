8e59773e63814a69e1dfa69e745ad096c22cbf9a
Farhan9696 <37727645+Farhan9696@users.noreply.github.com>
2019-08-28 15:20:10 +0530

    Create loop10.c

---
+++ loop10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    long long n;
+    int count = 0;
+    printf("Enter any number: ");
+    scanf("%lld", &n);
+    while(n != 0)
+    {
+        count++;
+        n /= 10;
+    }
+
+    printf("Total digits: %d", count);
+
+    return 0;
+}
